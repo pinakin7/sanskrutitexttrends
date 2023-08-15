c1dbdfd8d6794948953c552e6f9f1cb9cf49b5be
Preet Viradiya <pinakin548@gmail.com>
2023-08-15 11:36:07 +0530

    new

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -6,13 +6,31 @@ import {
   CssBaseline,
   Container,
   Paper,
+  Divider,
 } from "@mui/material";
 import "../css/testimonials.css";
 
 const testimonials = [
-  { id: 1, text: "Great experience working with this company." },
-  { id: 2, text: "The product exceeded my expectations." },
-  // Add more testimonials here
+  {
+    id: 1,
+    text: `" I'm so grateful to Sanskruti Text Trends for providing the best stitching threads for my fashion boutique. The threads are incredibly strong and reliable, making my designs stand the test of time. With their diverse range of threads, I always find the perfect match for my creations. Thank you for your exceptional products and excellent service! "`,
+    avatar: "https://bootdey.com/img/Content/avatar/avatar1.png",
+  },
+  {
+    id: 2,
+    text: `" I just had to share how much I love Sanskruti Text Trends' embroidery threads. The colors are so vibrant, and the threads work like a dream for my embroidery projects. I truly appreciate the high quality and reliability of your threads. You've won me over as a customer for life! "`,
+    avatar: "https://bootdey.com/img/Content/avatar/avatar2.png",
+  },
+  {
+    id: 3,
+    text: `" Sanskruti Text Trends has transformed our textile production with their top-notch machinery. The TFO machines and weaving machines have boosted our efficiency, and the results are fantastic. Your team's support and dedication are commendable. Thank you for making our work easier and more enjoyable! "`,
+    avatar: "https://bootdey.com/img/Content/avatar/avatar3.png",
+  },
+  {
+    id: 4,
+    text: `" Big shoutout to Sanskruti Text Trends for their exceptional industrial threads and carpet threads. These threads are super sturdy and have been a game-changer for our rug manufacturing business. Your attentive customer service is the icing on the cake. We're so glad we found you! "`,
+    avatar: "https://bootdey.com/img/Content/avatar/avatar4.png",
+  },
 ];
 
 const Testimonials = () => {
@@ -36,10 +54,11 @@ const Testimonials = () => {
   }, []);
 
   return (
-    <>
+    <div id="testimonials">
+      <Divider variant="middle" />
       <CssBaseline />
       <Paper elevation={3} className="testimonial-paper">
-        <Container maxWidth="md">
+        <Container maxWidth="auto">
           <Typography variant="h2" align="center" my={4} fontFamily="Pacifico">
             What our Clients say
           </Typography>
@@ -52,8 +71,21 @@ const Testimonials = () => {
                     index === currentIndex ? "active" : ""
                   }`}
                 >
-                  <Avatar alt="Anonymous" src="/path/to/avatar.png" style={{margin:"1rem"}} />
-                  <Typography variant="body1" paragraph style={{margin:"1rem", textAlign:"justify", padding:"0.5rem"}} className="testimonial-text">
+                  <Avatar
+                    alt="Anonymous"
+                    src={testimonial.avatar}
+                    style={{ margin: "1rem" }}
+                  />
+                  <Typography
+                    variant="body1"
+                    paragraph
+                    style={{
+                      margin: "1rem",
+                      textAlign: "justify",
+                      padding: "0.5rem",
+                    }}
+                    className="testimonial-text"
+                  >
                     {testimonial.text}
                   </Typography>
                   <Typography variant="caption" color="textSecondary">
@@ -65,7 +97,7 @@ const Testimonials = () => {
           </Box>
         </Container>
       </Paper>
-    </>
+    </div>
   );
 };
 
