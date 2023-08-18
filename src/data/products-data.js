0f6d4c823ea022164a03ace3dd3ff2c1406df0a6
Preet Viradiya <pinakin548@gmail.com>
2023-08-18 20:42:37 +0530

    new

--- src/data/products-data.js
+++ src/data/products-data.js
@@ -1,6 +1,21 @@
 import HighTenacity from "../img/threads/high-tenacity-yarn.jpg";
 import IndustrialThread from "../img/threads/industrial-thread.webp";
 import SewingThread from "../img/threads/sewing-thread.jpeg";
+import ZipStitchThread from "../img/threads/zip-stitch-thread.jpeg";
+import CarpetYarn from "../img/threads/carpet-yarn.webp";
+import EmbroiderYarn from "../img/threads/embroidery-yarn.jpeg";
+import TwistedWarp from "../img/weaving-yarn/twisted-warp.jpg";
+import TwistedWeft from "../img/weaving-yarn/twisted-weft.webp";
+import DyedWarp from "../img/weaving-yarn/dyed-warp.jpg";
+import DyedWeft from "../img/weaving-yarn/dyed-weft.jpg";
+import Fancy from "../img/weaving-yarn/fancy.jpeg";
+import Spun from "../img/weaving-yarn/spun.jpg";
+import Shirting from "../img/fabrics/shirting.webp";
+import Interlinings from "../img/fabrics/interlinings.webp";
+import JacquardFabric from "../img/fabrics/jacquard-fabric.jpg";
+import IndustrialTwister from "../img/textile-machines/industrial-twister.jpg";
+
+
 const categories = [
   {
     name: "Thread",
@@ -8,44 +23,44 @@ const categories = [
       { name: "High Tenacity Thread", image: HighTenacity },
       { name: "Industrial Thread", image: IndustrialThread },
       { name: "Sewing Thread", image: SewingThread },
-      { name: "Zip Stitch Thread", image: SewingThread },
-      { name: "Carpet Yarns", image: SewingThread },
-      { name: "Embroidery Yarns", image: SewingThread },
+      { name: "Zip Stitch Thread", image: ZipStitchThread },
+      { name: "Carpet Yarns", image: CarpetYarn },
+      { name: "Embroidery Yarns", image: EmbroiderYarn },
     ],
   },
   {
     name: "Weaving Yarn",
     products: [
-      { name: "Twisted Warp", image: "" },
-      { name: "Twisted Weft", image: "" },
-      { name: "Dyed Warp", image: "" },
-      { name: "Dyed Weft", image: "" },
-      { name: "Fancy Yarns", image: "" },
-      { name: "Spun Yarns", image: "" },
+      { name: "Twisted Warp", image: TwistedWarp },
+      { name: "Twisted Weft", image: TwistedWeft },
+      { name: "Dyed Warp", image: DyedWarp },
+      { name: "Dyed Weft", image: DyedWeft },
+      { name: "Fancy Yarns", image: Fancy },
+      { name: "Spun Yarns", image: Spun },
     ],
   },
   {
     name: "Fabrics",
     products: [
-      { name: "Shirting", image: "" },
-      { name: "Interlining", image: "" },
-      { name: "Jacquard Fabrics", image: "" },
+      { name: "Shirting", image: Shirting },
+      { name: "Interlining", image: Interlinings },
+      { name: "Jacquard Fabrics", image: JacquardFabric },
     ],
   },
   {
     name: "Textile Machines",
     products: [
-      { name: "TFO Machines", image: "" },
-      { name: "Pre Winder", image: "" },
-      { name: "Thread Yarn Plant", image: "" },
-      { name: "Spun TFO", image: "" },
-      { name: "High Bulk TFO", image: "" },
-      { name: "Cabling Machine", image: "" },
-      { name: "Industrial Twister", image: "" },
-      { name: "Soft Winder", image: "" },
-      { name: "Dyed Yarn Rewinder", image: "" },
-      { name: "Rapier Loom", image: "" },
-      { name: "Air Jet Loom", image: "" },
+      // { name: "TFO Machines", image: "" },
+      // { name: "Pre Winder", image: "" },
+      // { name: "Thread Yarn Plant", image: "" },
+      // { name: "Spun TFO", image: "" },
+      // { name: "High Bulk TFO", image: "" },
+      // { name: "Cabling Machine", image: "" },
+      { name: "Industrial Twister", image: IndustrialTwister },
+      // { name: "Soft Winder", image: "" },
+      // { name: "Dyed Yarn Rewinder", image: "" },
+      // { name: "Rapier Loom", image: "" },
+      // { name: "Air Jet Loom", image: "" },
     ],
   },
 ];
